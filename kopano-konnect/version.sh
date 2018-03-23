d76bb38b5754f8224fd6728934d34f1b5c0b0b76
Felix Bartels <felix@host-consultants.de>
2018-03-23 12:05:33 +0100

    fix version.sh to detect releases without an offset
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- kopano-konnect/version.sh
+++ kopano-konnect/version.sh
@@ -1,1 +1,1 @@
-curl -s 'https://download.kopano.io/community/konnect:/' | grep -oP 'href="/community/konnect:/kopano-konnect-\K[0-9]+\.[0-9]+\.[0-9]+\-[0-9]+' | sort -t. -rn -k1,1 -k2,2 -k3,3 | head -1
+curl -s 'https://download.kopano.io/community/konnect:/' | grep -oP 'href="/community/konnect:/kopano-konnect-\K[0-9]+\.[0-9]+\.[0-9]+\-[0-9]+|href="/community/konnect:/kopano-konnect-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -t. -rn -k1,1 -k2,2 -k3,3 | head -1
