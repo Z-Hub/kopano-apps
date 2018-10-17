11b22c1558881aa879f14162e54dd287ce8ab44f
Felix Bartels <felix@host-consultants.de>
2018-10-17 16:30:17 +0200

    commit some wip for the rest api app
    Signed-off-by: Felix Bartels <felix@host-consultants.de>

--- kopano-api/version.sh
+++ kopano-api/version.sh
@@ -1,2 +1,2 @@
-#curl -s 'https://download.kopano.io/community/konnect:/' | grep -oP 'href="/community/konnect:/kopano-konnect-\K[0-9]+\.[0-9]+\.[0-9]+\-[0-9]+|href="/community/konnect:/kopano-konnect-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -t. -rn -k1,1 -k2,2 -k3,3 | head -1
-echo latest
+commits=$(git rev-list --count master)
+echo 0.0.$commits
